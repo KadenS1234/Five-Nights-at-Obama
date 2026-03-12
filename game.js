a72675d2601cffcc5bff0b85e8fe38bf5612cd13
KadenS1234 <Kaden.Sarver@fairland.k12.oh.us>
2026-03-12 11:52:57 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,58 @@
+let hour = 12
+let obamaDistance = 5
+let doorClosed = false
+
+function updateStatus() {
+    document.getElementById("status").innerText =
+        "Time: " + hour + " AM | Obama distance: " + obamaDistance
+}
+
+function checkCamera() {
+
+    if (obamaDistance > 0) {
+        obamaDistance--
+    }
+
+    if (obamaDistance == 0 && !doorClosed) {
+        loseGame()
+    }
+
+    updateStatus()
+}
+
+function closeDoor() {
+    doorClosed = true
+    document.getElementById("status").innerText = "Door Closed!"
+}
+
+function gameLoop() {
+
+    hour++
+
+    if (hour == 6) {
+        winGame()
+    }
+
+    if (!doorClosed && Math.random() < 0.4) {
+        obamaDistance--
+    }
+
+    if (obamaDistance <= 0 && !doorClosed) {
+        loseGame()
+    }
+
+    updateStatus()
+
+}
+
+function loseGame() {
+    alert("Obama murked you!")
+    location.reload()
+}
+
+function winGame() {
+    alert("You survived the night!")
+    location.reload()
+}
+
+setInterval(gameLoop, 5000)
